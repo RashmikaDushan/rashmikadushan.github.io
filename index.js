9bd88005015e37b63d0236afc86af6995dcdac94
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-06-17 23:18:36 +0530

    cursor mod

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const text = document.getElementById("backgroundtext");
 text.scroll(200, 0);
+
+const cursorInner = document.getElementById("cursor-inner");
+const cursorOuter = document.getElementById("cursor-outer");
+
+window.addEventListener("mousemove",function (e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorInner.style.left = `${posX}px`;
+    cursorInner.style.top = `${posY}px`;
+
+    // cursorOuter.style.left = `${posX}px`;
+    // cursorOuter.style.top = `${posY}px`;
+
+    cursorOuter.animate({
+        left:`${posX}px`,
+        top:`${posY}px`
+    }, { duration: 300, fill:"forwards" });
+});
