ec19ace359e6e32caab25baf14a13f7eac4926cc
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-06-19 01:20:03 +0530

    scrolling + navtext + underline

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const text = document.getElementById("backgroundtext");
-text.scroll(200, 0);
+
+const undeline = document.getElementById('underline');
 
 const cursorInner = document.getElementById("cursor-inner");
 const cursorOuter = document.getElementById("cursor-outer");
 
+var cursorScale = document.querySelectorAll('.cursor-hover');
+
+let navlinks = document.querySelectorAll('.navtexts');
+let sections = document.querySelectorAll('.section');
+
+const header = document.getElementById('header');
+
+// place underline when first load
+let x = document.getElementById('navhome');
+undeline.style.left = `${x.offsetLeft}px`;
+
+// Background text
+text.scroll(200, 0);
+
+// Cursor movement
 window.addEventListener("mousemove", function (e) {
     const posX = e.clientX;
     const posY = e.clientY;
@@ -20,8 +36,7 @@ window.addEventListener("mousemove", function (e) {
     }, { duration: 150, fill: "forwards" });
 });
 
-var cursorScale = document.querySelectorAll('.cursor-hover');
-
+// Cursor hover effect
 cursorScale.forEach(link => {
     link.addEventListener('mouseleave', () => {
         cursorInner.classList.remove('grow-cursor');
@@ -32,3 +47,60 @@ cursorScale.forEach(link => {
         cursorOuter.classList.add('vanish-cursor');
     });
 })
+
+
+window.onscroll = () => {
+
+    // Header height change
+    if (window.scrollY > 10) {
+        header.classList.add('whitebar');
+    }
+    if (window.scrollY < 10) {
+        header.classList.remove('whitebar');
+    }
+
+    // Navbar highlihgt section effect
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top+300 >= offset && top+300 < offset + height){
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.getElementById(`nav${id}`).classList.add('active');
+
+                undeline.animate({
+                    left: `${document.getElementById(`nav${id}`).offsetLeft}px`,
+                }, { duration: 500, fill: "forwards", easing: "ease-in-out"}); // todo
+            })
+        }
+    });
+};
+
+// projectslink.onclick = () => {
+//     let ladingSection = document.getElementById('projects');
+//     let land = ladingSection.offsetTop+(ladingSection.offsetHeight/2);
+//     window.scrollTo(0, land);
+//     console.log(projectslink.offsetLeft);
+//     undeline.style.left = `${projectslink.offsetLeft}px`;
+// }
+
+navlinks.forEach(link => {
+    link.onclick = () => {
+        let ladingSection = document.getElementById(link.id.replace('nav', ''));
+        let land = ladingSection.offsetTop;
+
+        window.scrollTo({
+            top: land,
+            behavior: 'smooth'
+        });
+        // window.scrollTo(0, land);
+        // undeline.style.left = `${link.offsetLeft}px`;
+
+        undeline.animate({
+            left: `${link.offsetLeft}px`,
+        }, { duration: 300, fill: "forwards", easing: "ease-in-out"}); // todo
+    }
+})  
